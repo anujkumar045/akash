84a789996d559b9432cc01abe4fc40040839995a
anujkumar045 <prasadanujkumar045@gmail.com>
2025-11-11 17:59:26 +0530

    set

---
+++ cond.py
@@ -0,0 +1,10 @@
+# n=4
+# if n>=1:
+#     print("Given no. is +ve")
+
+# n=int(input("Enter any value :"))
+# if n>=1:
+#     print(f'given number {n} is +ve')
+# else:
+#     print(f'given number {n} is either zero or -ve')
+
