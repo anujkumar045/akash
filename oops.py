ab094ee97879e02c98d3b9be0cf0247b3ed8e285
anujkumar045 <prasadanujkumar045@gmail.com>
2025-12-05 18:19:17 +0530

    Variable

--- oops.py
+++ oops.py
@@ -75,15 +75,72 @@
 # print(obj.school_city,student.school_city)
 # obj.detail()
 
-class student:
-    def __init__(self,name,age,grade):
-        self.n=name
-        self.a=age
-        self.g=grade
-    def display(self):
-        print(self.n,self.a,self.g)
+# class student:
+#     def __init__(self,name,age,grade):
+#         self.n=name
+#         self.a=age
+#         self.g=grade
+#     def display(self):
+#         print(self.n,self.a,self.g)
+# obj=student('Anuj','21','B.E.')
+# obj.display()                #to call assigned values inside the class
+# print(obj.n,obj.a,obj.g)     #to access/call outside the class
+# student().display()
 
-obj=student('Anuj','21','B.E.')
-obj.display()                #to call assigned values inside the class
-print(obj.n,obj.a,obj.g)     #to access/call outside the class
-student().display()
+"""# variables which are object dependent(which change on changing of objects) are called as instance variable.
+#ex : name,email,contact
+#variables which are not object dependent are called as class variable.
+#ex : s.name,schoolcentercode
+#variables which are fully independent are called local variable.
+#ex : personalized 
+#Instance of class is called object"""
+
+'''Instance Variable'''
+class student:
+    def __init__(self,name,contact):
+        self.n=name
+        self.c=contact      #declaration inside constructor
+        print(self.n,self.c) #calling inside constructor
+    def add_new(self,roll_no):
+        self.r=roll_no           #declaration inside instance method
+    def display(self):
+        print(self.n,self.c,self.r,self.email)     #calling inside instance method
+obj=student('Anuj',7543080248)
+obj.add_new('CS0020')
+obj.email='anuj@gmail.com'   #declaration outside of the class
+obj.display()
+print(obj.n,obj.c,obj.r,obj.email)  #calling outside of the class
+obj1=student('V',9890808367)
+
+"""Class Variable"""
+'''Declaration and calling of class is using name of class.'''
+class student:
+    school_name='SHSS'
+    def __init__(self,name,roll_no):
+        self.n=name
+        self.r=roll_no
+        student.school_city='Bhopal'
+        print(student.school_city,student.school_name)
+    def add_new(self):
+        student.school_code=101
+        print(student.school_city,student.school_name,student.school_code,student.contact)
+student.contact=123455678
+obj=student('Anuj',123)
+obj.add_new()
+print(student.contact,student.school_name,student.school_city,student.school_code)
+print(obj.contact,obj.school_name,obj.school_city,obj.school_code)#This is accessable but we can't call
+                                                                    #using object in class variable method.
+
+"""Local variable"""
+'''Variable which are call in scope is called local variable.'''
+class student:
+    def __init__(self):
+        x=10
+        print(x)
+    def new(self):
+        y=20
+        z=y+10
+        print(z)
+        #print(x)
+obj=student()
+obj.new()
